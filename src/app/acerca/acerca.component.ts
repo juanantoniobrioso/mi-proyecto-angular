259aaa025d55033d4200db78ef95ad51b1f3017f
Juan Antonio.Brioso <juanantoniobrioso.24@campuscamara.es>
2025-11-21 14:12:19 +0100

    viernes 21 nov

--- src/app/acerca/acerca.component.ts
+++ src/app/acerca/acerca.component.ts
@@ -7,6 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AcercaComponent {
 
+contador : number = 0;
+resultado : string="";
+
+
+opcionSeleccionada: string = '';
+actualizarOpcion(evento: Event){
+  const selectElement = evento.target as HTMLSelectElement;
+  this.opcionSeleccionada = selectElement.value;
+}
+
+
+mensajeFoco: string = '';
+mostrarFoco() {
+this.mensajeFoco = 'Campo enfocado';
+}
+quitarFoco() {
+this.mensajeFoco = 'Campo desenfocado';
+}
+
+
+
+incrementar(){
+  this.contador++;
+}
+decrementar(){
+  this.contador--;
+}
+
+actualizartexto(evento: Event){
+  const inputElement = evento.target as HTMLInputElement;
+  this.resultado=inputElement.value;
+}
+
+
+
+
+
 
 numero1 : number = 10;
 numero2 : number = 0;
@@ -22,3 +59,5 @@ obtenerMensaje(): string {
 
 
 }
+
+
